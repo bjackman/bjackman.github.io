4e2f6eefe0d5f6408a769da6fa0dd4f57ff0f973
Brendan Jackman <brendan.jackman@bluwireless.co.uk>
2019-08-30 14:52:55 +0700

    Reduce td padding

--- assets/js/pangrams.js
+++ assets/js/pangrams.js
@@ -218,6 +218,7 @@ function updateTable() {
     let tdEnter = td.enter().append("td")
         .style("display", "inline-block")
         .style("font-family", "monospace")
+        .style("padding", "8px")
         .text(d => `${d.letter}: `);
     tdEnter.append("span").attr("class", "claimed-count").text(genClaimedCountText);
     tdEnter.append("span").style("cursor", "pointer").text("+").on("click", d => {
