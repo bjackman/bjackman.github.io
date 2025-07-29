5c42a6f9489de7a5f8d932e17afdc7ef2fabede7
Brendan Jackman <bhenryj0117@gmail.com>
2025-07-29 12:20:14 +0200

    nix: Add package for site

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    # Make ${./.} include submodule contents
+    self.submodules = true;
   };
 
   outputs =
@@ -13,6 +15,23 @@
     in
     {
       formatter."${system}" = pkgs.nixfmt-tree;
+
+      packages."${system}" = rec {
+        site = pkgs.runCommand "site" {
+          nativeBuildInputs = [ pkgs.hugo ];
+        } ''
+          # Can't get Hugo to not try to modify the source tree, so copy it
+          # somewhere writable.
+          writableSrc=$(mktemp -d)
+          cp -R ${./.}/* "$writableSrc"
+
+          hugo build \
+            --cacheDir $(mktemp -d) \
+            --destination $out \
+            --source "$writableSrc"
+        '';
+        default = site;
+      };
 
       devShells."${system}".default = pkgs.mkShell {
         packages = [ pkgs.hugo ];
